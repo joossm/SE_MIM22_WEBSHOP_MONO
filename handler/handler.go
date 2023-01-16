bfb0bbd700f1deb0c1cbe2b2e80475617c14cd70
Matthias Jooß <Jooss.Matthias@gmail.com>
2023-01-16 15:01:19 +0100

    DATABASE WORKS

--- handler/handler.go
+++ handler/handler.go
@@ -17,7 +17,7 @@ const post = "POST"
 func InitDatabase(responseWriter http.ResponseWriter, request *http.Request) {
 	db := openDB()
 	defer closeDB(db)
-	fmt.Println(db)
+	fmt.Println("init db was executed")
 
 	/*fmt.Println("Creating table books...")
 	err, _ := db.Query("USE books")
@@ -25,11 +25,11 @@ func InitDatabase(responseWriter http.ResponseWriter, request *http.Request) {
 		fmt.Printf("Error Use Books: %s", err)
 	}*/
 
-	err, _ := db.Exec("CREATE TABLE IF NOT EXIST `books` ( `Id` int(11) NOT NULL, `Titel` varchar(45) DEFAULT NULL, `EAN` varchar(45) DEFAULT NULL, `Content` varchar(45) DEFAULT NULL, `Price` float DEFAULT NULL, PRIMARY KEY (`Id`) ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;")
+	err, _ := db.Exec("CREATE TABLE IF NOT EXISTS `books` ( `Id` int(11) NOT NULL, `Titel` varchar(45) DEFAULT NULL, `EAN` varchar(45) DEFAULT NULL, `Content` varchar(45) DEFAULT NULL, `Price` float DEFAULT NULL, PRIMARY KEY (`Id`) ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;")
 	if err != nil {
 		log.Printf("Error creating table: %s", err)
 	}
-	err, _ = db.Exec("CREATE TABLE IF NOT EXIST `users` ( `Id` int(11) NOT NULL, `Username` varchar(45) DEFAULT NULL, `Password` varchar(45) DEFAULT NULL, `Firstname` varchar(45) DEFAULT NULL, `Lastname` varchar(45) DEFAULT NULL, `Housenumber` varchar(45) DEFAULT NULL, `Street` varchar(45) DEFAULT NULL, `Zipcode` varchar(45) DEFAULT NULL, `City` varchar(45) DEFAULT NULL, `Email` varchar(45) DEFAULT NULL, `Phone` varchar(45) DEFAULT NULL, PRIMARY KEY (`Id`) ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;")
+	err, _ = db.Exec("CREATE TABLE IF NOT EXISTS `users` ( `Id` int(11) NOT NULL, `Username` varchar(45) DEFAULT NULL, `Password` varchar(45) DEFAULT NULL, `Firstname` varchar(45) DEFAULT NULL, `Lastname` varchar(45) DEFAULT NULL, `Housenumber` varchar(45) DEFAULT NULL, `Street` varchar(45) DEFAULT NULL, `Zipcode` varchar(45) DEFAULT NULL, `City` varchar(45) DEFAULT NULL, `Email` varchar(45) DEFAULT NULL, `Phone` varchar(45) DEFAULT NULL, PRIMARY KEY (`Id`) ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;")
 	if err != nil {
 		log.Printf("Error creating table: %s", err)
 	}
@@ -82,7 +82,9 @@ func Register(responseWriter http.ResponseWriter, request *http.Request) {
 	default:
 		responseWriter.Write([]byte("THIS IS A POST REQUEST"))
 	case post:
+		fmt.Println("Register was executed")
 		if request.Body != nil {
+			fmt.Println("Body not nil")
 			body, _ := ioutil.ReadAll(request.Body)
 			user := model.User{}
 			jsonErr := json.Unmarshal(body, &user)
@@ -90,26 +92,42 @@ func Register(responseWriter http.ResponseWriter, request *http.Request) {
 				responseWriter.Write([]byte("{ERROR}"))
 				return
 			}
+			fmt.Println("No json error")
 			db := openDB()
 			defer closeDB(db)
 			result, err := db.Query("SELECT Username FROM users WHERE Username = ?", user.Username)
+			fmt.Println("result: ", result)
 			errorHandler(err)
+			fmt.Println("Query executed")
 			var users []model.User
-			if result != nil {
+			if result.Next() == true {
 				for result.Next() {
 					var user model.User
 					err = result.Scan(&user.Id, &user.Username, &user.Password)
-					fmt.Println(user.Username, user.Password)
+					fmt.Println("user: ", user.Username, user.Password)
 					users = append(users, user)
 				}
-			}
-
-			if users != nil {
-				responseWriter.Write([]byte("{already exists}"))
-				return
+				if users != nil {
+					responseWriter.Write([]byte("{already exists}"))
+					return
+				}
 			} else {
-				db.Query("INSERT INTO users (Username, Password, Firstname, Lastname, HouseNumber, Street, ZipCode, City, Email, Phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-					user.Username, user.Password, user.Firstname, user.Lastname, user.HouseNumber, user.Street, user.ZipCode, user.City, user.Email, user.Phone)
+				// GET MAX ID
+				result, err := db.Query("SELECT MAX(Id) FROM users")
+				errorHandler(err)
+				var maxId int
+				if result != nil {
+					for result.Next() {
+						err = result.Scan(&maxId)
+						errorHandler(err)
+					}
+				}
+				maxId++
+				fmt.Println("result is nil | execute insert")
+				res, err := db.Query("INSERT INTO users (Id, Username, Password, Firstname, Lastname, HouseNumber, Street, ZipCode, City, Email, Phone) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+					maxId, user.Username, user.Password, user.Firstname, user.Lastname, user.HouseNumber, user.Street, user.ZipCode, user.City, user.Email, user.Phone)
+				fmt.Println(res)
+				errorHandler(err)
 				responseWriter.Write([]byte("{true}"))
 				return
 			}
@@ -217,13 +235,21 @@ func closeDB(db *sql.DB) {
 }
 
 func openDB() *sql.DB {
-	db, err := sql.Open("mysql", "admin:password@tcp(mysql:3306)/books")
+	fmt.Println("Opening DB 2")
+	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/books")
 	fmt.Println(db.Ping())
+	fmt.Println(db.Stats())
+	fmt.Println("Opening DB 3")
+	db.SetMaxIdleConns(0)
+	db, err = sql.Open("mysql", "root:root@tcp(docker.for.mac.localhost:3306)/books")
+	db.SetMaxIdleConns(0)
+	fmt.Println(db.Ping())
+	fmt.Println(db.Stats())
 	errorHandler(err)
 	return db
 }
 func errorHandler(err error) {
 	if err != nil {
-		print(err)
+		fmt.Println(err)
 	}
 }
