5da8b7b3dbd7dd0b4a2a86e8ffc7bf6a13c2aecc
Matthias Jooß <Jooss.Matthias@gmail.com>
2023-01-21 16:18:23 +0100

    handler upgrade

--- handler/handler.go
+++ handler/handler.go
@@ -354,4 +354,8 @@ func errorHandler(err error) {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).Header().Set("Referer", "No-referrer")
+
 }
